38d9e1a88b53637cec8e400d20f46bc0cdc0b5bf
Gabriel Carvalho <gabrielcoelhoneves@hotmail.com>
2022-09-12 20:05:13 -0300

    aula07 e aula08

---
+++ aula07/index.js
@@ -0,0 +1,7 @@
+console.log('Gabriel Carvalho');
+console.log("Gabriel Carvalho");
+console.log(`Gabriel Carvalho`); //template strings 
+console.log("Gabriel 'Carvalho'"); 
+
+console.log(123456, 15.85); //Number
+console.log(123, 13.3, 'Gabriel o Carvalho');
