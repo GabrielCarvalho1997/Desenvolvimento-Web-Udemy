03a81a1fe83b3a4cc67f674afb7032b2f9339ae7
Gabriel Carvalho <gabrielcoelhoneves@hotmail.com>
2022-09-13 11:55:07 -0300

    aula23 - exercícios

---
+++ aula23 - exercícios/index.js
@@ -0,0 +1,17 @@
+let varA = 'A'; //B
+let varB = 'B'; //C
+let varC = 'C'; //A
+
+//Maneira antiga
+const varATemp = varA;
+varA = varB;
+varB = varC;
+varC = varATemp;
+
+/*Maneira nova
+[varA, varB, varC] = [varB, varC, varA]
+*/
+
+console.log(varA, varB, varC);
+
+
