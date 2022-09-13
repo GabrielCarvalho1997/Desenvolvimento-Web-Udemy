20f154a45a3e3365bce4b8ca7a07a6b1249fda0f
Gabriel Carvalho <gabrielcoelhoneves@hotmail.com>
2022-09-13 09:40:49 -0300

    aula18

---
+++ aula18/index.js
@@ -0,0 +1,6 @@
+//Let vs Var
+
+var nome = 'Luiz'; // o var permite declarar a variável novamente, substituindo seu valor
+var nome = 'Gabriel'; // o var permite declarar a variável novamente, substituindo seu valor
+
+let nome = 'Leslye'; // let não permite redeclarar a variável
