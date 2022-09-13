54339bf36ef40e07715d40dc37f7e5bcecdeea93
Gabriel Carvalho <gabrielcoelhoneves@hotmail.com>
2022-09-13 09:13:40 -0300

    aula14

---
+++ aula14/index.js
@@ -0,0 +1,11 @@
+const nome = 'João';
+console.log(nome);
+
+const primeiroNumero = 5;
+const segundoNumero = 3;
+const resultado = primeiroNumero * segundoNumero;
+console.log(resultado);
+
+console.log(typeof nome);
+console.log(typeof resultado);
+
