1932f495ddc714a869a678b61e94ab364fdf8fc9
Gabriel Carvalho <gabrielcoelhoneves@hotmail.com>
2022-09-13 10:39:03 -0300

    aula20

---
+++ aula20/index.js
@@ -0,0 +1,27 @@
+/*
+Aritiméticos:
+
++ -> faz adição e concatenação 
+- -> subtração
+/ -> divisão
+* -> multiplicaçãp
+** -> potência
+% -> resto da divisão
+() - parênteses igual na matemática
+
+- a ordem é a mesma da matemática -
+
+++ -> adiciona uma unidade no valor da variável - pode ser pré ou pós fixado
+-- -> subtrai uma unidade no valor da variável - pode ser pré ou pós fixado
+
++= -> adiciona qualquer valor que definir no valor da variável
+-= -> subtrai qualquer valor que definir no valor da variável
+*= -> multiplica qualquer valor que definir no valor da variável
+/= -> divide qualquer valor que definir no valor da variável
+**= -> potencia qualquer valor que definir no valor da variável
+
+parseInt -> converte para um número inteiro
+parseFloat -> converte para um número real - com decimal
+Number() -> converte para um número - no geral
+
+*/
