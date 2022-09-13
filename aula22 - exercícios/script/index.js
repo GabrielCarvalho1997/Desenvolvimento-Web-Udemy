7e480e26d42cad5ab10db3d948faa6c873e645c2
Gabriel Carvalho <gabrielcoelhoneves@hotmail.com>
2022-09-13 11:38:00 -0300

    aula22 - exercícios

---
+++ aula22 - exercícios/script/index.js
@@ -0,0 +1,5 @@
+const num1 = window.prompt('Digite um número.');
+const num2 = window.prompt('Digite outro número.');
+const resultado = parseFloat(num1) + parseFloat(num2)
+
+window.alert(`O resultado da soma dos número é ${resultado}`)
