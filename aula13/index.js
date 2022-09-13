1f8f453102a2d905db2757d1606487d91f873660
Gabriel Carvalho <gabrielcoelhoneves@hotmail.com>
2022-09-13 08:55:28 -0300

    aula 13

---
+++ aula13/index.js
@@ -0,0 +1,14 @@
+let nome = 'João'; //String
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nome, 'em 2015');
+console.log('O filho de', nome, 'se chama Eduardo');
+
+// Não se pode criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase - primeira palavra com letra minúscula e as próximas com maiúsculas
+// Case-sensitive - sensíveis a letras maiúsculas e minúsculas
