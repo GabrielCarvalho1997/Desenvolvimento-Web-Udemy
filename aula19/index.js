2f0772c095415405fbefc41df329a444821d098a
Gabriel Carvalho <gabrielcoelhoneves@hotmail.com>
2022-09-13 10:05:10 -0300

    aula19

---
+++ aula19/index.js
@@ -0,0 +1,6 @@
+let nome = 'Gabriel';  //String
+let idade = 25;        //Number
+let peso = 78.5;       //Number - com ponto flutuante
+let sobrenome;         //undefined - não aponta para local nenhum na memoria
+let altura = null;     //Nulo - não aponta para local nenhum na memória, porém pode apontar
+let gostoso = true;    //boolean - pode assumir true ou false - (lógico)
