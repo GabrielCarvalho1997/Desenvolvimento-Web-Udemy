2db1cba44bf80326ade70205a75d3d8d54c431a2
Gabriel Carvalho <gabrielcoelhoneves@hotmail.com>
2022-09-14 08:57:30 -0300

    aula27

---
+++ aula27/index.js
@@ -0,0 +1,4 @@
+let num1 = 9.54578
+let num2 = Math.floor(num1); //arrendonda para baixo em valor inteiro
+let num3 = Math.ceil(num1); //arrendonda pra cima em valor inteiro
+let num4 = Math.round(num1); //arredonda para um valor inteiro    
