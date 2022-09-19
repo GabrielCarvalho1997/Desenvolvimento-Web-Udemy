175f65eef795266fb3b1fb8dbfa6fccf8ecd890d
Gabriel Carvalho <gabrielcoelhoneves@hotmail.com>
2022-09-19 11:07:28 -0300

    aula61 - exercícios

---
+++ aula61 - exercícios/index.js
@@ -0,0 +1,9 @@
+function ePaisagem(largura, altura) {
+    return largura > altura ? true : false
+}
+
+console.log(ePaisagem(1980, 1080));
+
+const ePaisagem2 = (largura2, altura2) => largura2 > altura2;
+
+console.log(ePaisagem2(1000, 1080));
